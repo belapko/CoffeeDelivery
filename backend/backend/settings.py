de913cde44be515d282a553833dc61bb191d58bc
belapko <belapko23@gmail.com>
2022-09-24 21:16:59 +0300

    created products in react

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -46,11 +46,11 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
@@ -119,15 +119,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ORIGIN_ALLOW_ALL = False
-CORS_ORIGIN_WHITELIST = (
-    'https://localhost:3000',
-    'https://127.0.0.1:3000',
-)
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+]
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,33 @@
 import React from "react";
 import Header from "./components/Header";
 import Carousel from "./components/Carousel";
-import Products from "./components/Products";
+import ProductsList from "./components/Products";
 import "bootstrap/dist/css/bootstrap.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import ProductsService from "./API/ProductsService";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      products: [],
-      productCategories: [],
-    };
+function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  async function fetchProducts() {
+    const products = await ProductsService.getAll();
+    setProducts(products);
   }
 
-  render() {
-    return (
-      <body>
-        <div style={{height: 60 + 'px'}}>
-          <Header/>
-        </div>
-        
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-9">
-                <Carousel/>
-            </div>
-            <div className="row">
-                <Products />
-            </div>
-          </div>
-        </div>
-      </body>
-    );
-  }
+  return (
+    <body>
+      <Header />
+
+      <Carousel />
+
+      <ProductsList products={products} />
+    </body>
+  );
 }
 
 export default App;
